85e66233cf9b84d8ee4fa22c048d51ad67555710
JustinYGGVT <97996205+JustinYGGVT@users.noreply.github.com>
2022-02-24 19:21:20 +0800

    admin index update

--- admin-index.php
+++ admin-index.php
@@ -47,6 +47,7 @@
       <thead>
         <tr>
             <th>Product ID</th>
+            <th>Image</th>
             <th>Product Name</th>
             <th>Category</th>
             <th>Price</th>
@@ -56,22 +57,25 @@
         </tr>
       </thead>
       <tbody>
-        
+      <form>
         <tr>
         <?php foreach ($product->getProducts() as $product): ?>
           <th scope="row"><?php echo $product['product_id']; ?></th>
+          <td><?php echo $product['image']; ?></td>
           <td><?php echo $product['product_name']; ?></td>
           <td><?php echo $product['category_id']; ?></td>
           <td><?php echo $product['price']; ?></td>
           <td><?php echo $product['quantity']; ?></td>
           <td><?php echo $product['description']; ?></td>
-          <td>    <button href="delete-product.php?id=<?php echo product['cart_id']; ?>"</button>  
+          <td><a href ="admin-update-product.php?id=<?php echo $product['product_id']; ?>">Update</a>
                   <button onclick="ClearData()">Remove</button>  </td>
         </tr>
         <?php endforeach; ?>
+        </form>
       </tbody>
     </table>
     
+
 
 <?php include 'includes/footer.php'; ?>
 
---
+++ admin-update-product.php
@@ -0,0 +1,40 @@
+<?php 
+    include 'includes/header2.php';
+    include 'config/functions.php';
+?>
+<div style="margin: 100px"></div>
+<div class="container">
+    <table class="table table-striped table-dark">
+      <thead>
+        <tr>
+            <th>Product ID</th>
+            <th>Image</th>
+            <th>Product Name</th>
+            <th>Category</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        
+        <tr>
+        <?php foreach ($product->getProducts($_GET['id']) as $product): ?>
+          <th scope="row"><?php echo $product['product_id']; ?></th>
+          <td><input type="text" name="image value="<?php echo $product['image']; ?>"></td>
+          
+          <td><input type="text" name="product_name" value="<?php echo $product['product_name']; ?>"></td>
+          <td><input type="text" name="category_id" value="<?php echo $product['category_id']; ?>"></td>
+          <td><input type="text" name="price" value="<?php echo $product['price']; ?>"></td>
+          <td><input type="text" name="quantity[]" value="<?php echo $product['quantity']; ?>"></td>
+          <td><input type="text" name="description" value="<?php echo $product['description']; ?>"></td>
+
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+      <td><button onclick="UpdateData()">Update</button></td>
+    </table>
+        </div>   
+
+
+
